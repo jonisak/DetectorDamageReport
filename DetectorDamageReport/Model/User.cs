a5a0d7569c2c76d2adbe83e8224d9203d57cadb3
Jonas Isaksson <jonisak@gmail.com>
2019-05-07 20:07:15 +0000

    init

---
+++ DetectorDamageReport/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DetectorDamageReport.Model
+{
+    public class User
+    {
+        public long UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsBanned { get; set; }
+
+    }
+}
